cac90160e18bedb77e894325d36a82a1a9821e91
echaoo <1299529160@qq.com>
2016-02-28 14:12:48 +0800

    解密，登录完善

--- src/main/java/com/lihuanyu/repairsystem/conflg/DevConfig.java
+++ src/main/java/com/lihuanyu/repairsystem/conflg/DevConfig.java
@@ -6,7 +6,7 @@ package com.lihuanyu.repairsystem.conflg;
 public class DevConfig {
     public static String client_id = "ae4a014d9e91e119";
     public static String redirect_uri = "http://f.yiban.cn/iapp28905";
-    public static String AppSecret = "acba6316e2bda87a6be52bb9e5a5e215\n";
+    public static String AppSecret = "acba6316e2bda87a6be52bb9e5a5e215";
 
     public static String adminUsername = "admin";
     public static String admiPassword = "admin";
--- src/main/java/com/lihuanyu/repairsystem/controller/LoginController.java
+++ src/main/java/com/lihuanyu/repairsystem/controller/LoginController.java
@@ -1,9 +1,11 @@
 package com.lihuanyu.repairsystem.controller;
 
 import com.lihuanyu.repairsystem.conflg.DevConfig;
+import com.lihuanyu.repairsystem.util.MCrypt;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by echao on 2016/2/28.
@@ -12,13 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
 
     @RequestMapping(value = "/",method = RequestMethod.GET,params = "verify_request")
-    public String repariSystem() throws Exception{
-        return "index";
+    public String repariSystem(String verify_request) throws Exception{
+        MCrypt mCrypt = new MCrypt();
+        String output = new String(mCrypt.decrypt(verify_request));
+        return output;
     }
 
     @RequestMapping(value = "/",method = RequestMethod.GET)
-    public String testOauth(){
-
-        return "redirect:https://openapi.yiban.cn/oauth/authorize?client_id="+ DevConfig.client_id + "&redirect_uri=" + DevConfig.redirect_uri;
+    public ModelAndView testOauth(){
+        ModelAndView mv = new ModelAndView("redirect:https://openapi.yiban.cn/oauth/authorize?client_id="+ DevConfig.client_id + "&redirect_uri=" + DevConfig.redirect_uri);
+        return mv;
     }
 }
