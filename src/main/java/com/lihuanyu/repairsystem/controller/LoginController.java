531bc53f139d7caed758b557c59cd4fdcaea7dac
echaoo <1299529160@qq.com>
2016-03-01 09:27:05 +0800

    判断是否登陆

--- src/main/java/com/lihuanyu/repairsystem/controller/LoginController.java
+++ src/main/java/com/lihuanyu/repairsystem/controller/LoginController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpSession;
+
 /**
  * Created by echao on 2016/2/28.
  */
@@ -20,20 +22,26 @@ public class LoginController {
     @Autowired
     private SaveSessionService saveSessionService;
 
+    @Autowired
+    private HttpSession httpsession;
+
     @RequestMapping(value = "/",method = RequestMethod.GET,params = "verify_request")
     public String repariSystem(String verify_request) throws Exception{
         MCrypt mCrypt = new MCrypt();
         String output = new String(mCrypt.decrypt(verify_request));
         Gson gson = new Gson();
         SessionUser sessionUser = gson.fromJson(output, SessionUser.class);
-        
         saveSessionService.saveSession(sessionUser);
-
         return output;
     }
 
     @RequestMapping(value = "/",method = RequestMethod.GET)
     public ModelAndView testOauth(){
+        String userid = (String) httpsession.getAttribute("userid");
+        if(userid!=null){
+            ModelAndView mv1 = new ModelAndView("index");
+            return mv1;
+        }
         ModelAndView mv = new ModelAndView("redirect:https://openapi.yiban.cn/oauth/authorize?client_id="+ DevConfig.client_id + "&redirect_uri=" + DevConfig.redirect_uri);
         return mv;
     }
